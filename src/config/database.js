b5d040cd09593ca4d370980621c30b3d639e0455
AdamsEric <eric.adsbrito@live.com>
2020-10-20 19:46:43 -0300

    Adição de controle de ambiente para database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'root',
-  database: 'mymangacollection',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   logging: false,
   define: {
     timestamps: true,
